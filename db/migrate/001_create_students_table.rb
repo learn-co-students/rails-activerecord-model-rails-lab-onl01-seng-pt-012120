154d61eba777c39195b25124a66755b526277c82
danielsun <danielsun12345@gmail.com>
2020-05-21 16:48:49 -0700

    Done.

--- db/migrate/001_create_students_table.rb
+++ db/migrate/001_create_students_table.rb
@@ -1,4 +1,4 @@
-class CreateStudents < ActiveRecord::Migration
+class CreateStudentsTable < ActiveRecord::Migration
     def change
       create_table :students do |t|
         t.string :first_name
